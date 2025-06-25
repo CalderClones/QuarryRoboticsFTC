3090f1a3da2b826030dd57e9c24988b840d880e5
CalderClones <edgeleyl@caldies.co.uk>
2025-06-25 22:40:58 +0100

    We're Going to Nationals. Wish us luck!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryAction.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.nio.channels.ClosedByInterruptException;
+
+public class TelemetryAction implements Action {
+    private boolean initialized = false;
+    private String line;
+    private Telemetry telemetry;
+
+    public TelemetryAction(Telemetry telemetry, String line) {
+        this.line = line;
+        this.telemetry = telemetry;
+    }
+
+    @Override
+    public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+        if (!initialized) {
+            telemetry.addLine(line);
+            telemetry.update();
+            initialized = true;
+        }
+        telemetryPacket.put("Telemetry Posted", line);
+        return false;
+    }
+}
